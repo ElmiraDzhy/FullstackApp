e8bbd792d69e62ae154a801b13c311f8e6924a38
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-29 10:51:50 +0300

    Task resolve

--- server/controllers/User.controller.js
+++ server/controllers/User.controller.js
@@ -1,17 +1,40 @@
 const {User} = require('../models');
 
 module.exports.signUp = async (req, res, next) => {
-
+    try {
+        const {body} = req;
+        const newUser = await User.create(body);
+        res.status(201).send({data: newUser});
+    } catch (err) {
+        next(err)
+    }
 }
 
 module.exports.signIn = async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 }
 
 module.exports.getOne = async (req, res, next) => {
+    try {
+        const {params: {userId}} = req;
+        const userInstance = await User.findById(userId);
+        res.status(200).send({data: userInstance});
 
+    } catch (err) {
+        next(err)
+    }
 }
 
 module.exports.deleteOne = async (req, res, next) => {
-
+    try {
+        const {params: {userId}} = req;
+        const userInstance = await User.findByIdAndDelete(userId);
+        res.status(200).send({data: userInstance});
+    } catch (err) {
+        next(err)
+    }
 }
