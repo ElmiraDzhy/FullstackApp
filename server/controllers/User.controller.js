a16bf9f7b8e81091934c94e84b7a042bb3b8e74f
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-29 10:23:06 +0300

    User router done

---
+++ server/controllers/User.controller.js
@@ -0,0 +1,17 @@
+const {User} = require('../models');
+
+module.exports.signUp = async (req, res, next) => {
+
+}
+
+module.exports.signIn = async (req, res, next) => {
+
+}
+
+module.exports.getOne = async (req, res, next) => {
+
+}
+
+module.exports.deleteOne = async (req, res, next) => {
+
+}
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const userRouter = express.Router()
-const UserController = require('../controllers/User.controller')
+const express = require('express');
+const userRouter = express.Router();
+const UserController = require('../controllers/User.controller');
 
+userRouter.post('/sign-up', UserController.signUp); //signUp
+userRouter.post('/sign-in', UserController.signUp); //signIn
+userRouter.get('/userId', UserController.getOne);
+userRouter.delete('/userId', UserController.deleteOne);
 
 module.exports = userRouter;
