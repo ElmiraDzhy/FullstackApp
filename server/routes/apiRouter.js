5fe755e318feda1cf09a81275799dcb4a9c6e432
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-29 10:07:53 +0300

    Add chatRouter.js file

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userRouter = require('./userRouter');
+const chatRouter = require('./chatRouter');
 
 const apiRouter = express.Router();
 
 apiRouter.use('/users', userRouter);
+apiRouter.use('/chats', chatRouter);
+// apiRouter.use('/messages', );
 
 module.exports = apiRouter;
