278c935691e7bbbae6f4c62c01a68a30acc783fb
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-29 11:36:26 +0300

    Task resolve #3

--- server/controllers/Chat.controller.js
+++ server/controllers/Chat.controller.js
@@ -1,4 +1,4 @@
-const {Chat} = require('../models');
+const {Chat, Message} = require('../models');
 
 module.exports.createChat = async (req, res, next) => {
     try {
@@ -41,9 +41,14 @@ module.exports.getChatWithUsers = async (req, res, next) => {
 
 module.exports.addMessage = async (req, res, next) => {
     try {
-
+        const {body, params: {chatId}} = req;
+        const chatInstance = await Chat.findById(chatId);
+        const newMsgInstance = await Message.create(body);
+        chatInstance.messages.push(newMsgInstance);
+        await chatInstance.save();
+        res.status(201).send({data: chatInstance})
     } catch (err) {
-
+        next(err);
     }
 }
 
@@ -56,8 +61,20 @@ module.exports.getAllUserChats = async (req, res, next) => {
         res.status(200).send({data: userChats});
 
     } catch (err) {
+        next(err);
 
     }
 }
 
+module.exports.getChatWithMessages = async (req, res, next) => {
+    try {
+        const {params: {chatId}} = req;
+        const result = await Chat.findById(chatId).populate('messages')
+        res.status(200).send({data: result});
+
+    } catch (err) {
+        next(err)
+
+    }
+}
 //todo (optional): delete user from chat and delete chat
--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -4,8 +4,9 @@ const ChatController = require('../controllers/Chat.controller');
 
 chatRouter.post('/', ChatController.createChat);
 chatRouter.post('/:chatId', ChatController.addMessage);
-chatRouter.get('/:chatId', ChatController.getChatWithUsers);
+chatRouter.get('/:chatId/users', ChatController.getChatWithUsers);
 chatRouter.get('/:userId/all', ChatController.getAllUserChats);
 chatRouter.put('/:chatId/:userId', ChatController.addUserToChat);
+chatRouter.get('/:chatId', ChatController.getChatWithMessages)
 
 module.exports = chatRouter;
