a771c997bc4dfa4e4c753ef97e143e972643a4f8
ElyaDzhy <elmira.freshcode@gmail.com>
2023-06-29 10:28:25 +0300

    Chat router done

---
+++ server/controllers/Chat.controller.js
@@ -0,0 +1,21 @@
+module.exports.createChat = async (req, res, next) => {
+
+}
+
+module.exports.addUserToChat = async (req, res, next) => {
+
+}
+
+module.exports.getChatWithUsers = async (req, res, next) => {
+
+}
+
+module.exports.addMessage = async (req, res, next) => {
+
+}
+
+module.exports.getAllUserChats = async (req, res, next) => {
+
+}
+
+//todo (optional): delete user from chat and delete chat
--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const chatRouter = express.Router();
+const ChatController = require('../controllers/Chat.controller');
 
+chatRouter.post('/', ChatController.createChat);
+chatRouter.post('/:chatId', ChatController.addMessage);
+chatRouter.get('/:chatId', ChatController.getChatWithUsers);
+chatRouter.get('/:userId/all', ChatController.getAllUserChats);
+chatRouter.put('/:chatId/:userId', ChatController.addUserToChat);
 
 module.exports = chatRouter;
