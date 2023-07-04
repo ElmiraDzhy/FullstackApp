68af304d67d7313f6054e900b4f430437df87297
ElyaDzhy <elmira.freshcode@gmail.com>
2023-07-04 11:06:30 +0300

    Add functions for RefreshToken

--- server/services/tokenService.js
+++ server/services/tokenService.js
@@ -3,17 +3,30 @@ const {promisify} = require('node:util');
 
 const promisifyJWTSing = promisify(jwt.sign);
 const promisifyJWTVerify = promisify(jwt.verify);
-const SECRET_VALUE = 'super-secret';
-const ACCESS_TOKEN = 60;
 
-module.exports.createToken = async ({userId, email}) => {
-    const token = await promisifyJWTSing({userId, email}, SECRET_VALUE, {
-        expiresIn: ACCESS_TOKEN
+const ACCESS_SECRET_VALUE = 'super-secret';
+const REFRESH_SECRET_VALUE = 'super-secret-refresh';
+const ACCESS_TIME = 60;
+const REFRESH_TIME = 60 * 60;
+
+module.exports.createAccessToken = async ({userId, email}) => {
+    const token = await promisifyJWTSing({userId, email}, ACCESS_SECRET_VALUE, {
+        expiresIn: ACCESS_TIME
     });
-
     return token;
 }
 
-module.exports.verifyToken = async(token) => {
-    return promisifyJWTVerify(token, SECRET_VALUE);
+module.exports.createRefreshToken = async ({userId, email}) => {
+    const token = await promisifyJWTSing({userId, email}, REFRESH_SECRET_VALUE, {
+        expiresIn: REFRESH_TIME
+    });
+    return token;
+}
+
+module.exports.verifyAccessToken = async (token) => {
+    return promisifyJWTVerify(token, ACCESS_SECRET_VALUE);
+}
+
+module.exports.verifyRefreshToken = async (token) => {
+    return promisifyJWTVerify(token, REFRESH_SECRET_VALUE);
 }
