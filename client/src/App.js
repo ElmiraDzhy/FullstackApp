7dd112230b301c9d7f743f6e575b1c2bbab69535
ElyaDzhy <elmira.freshcode@gmail.com>
2023-07-07 09:18:09 +0300

    Refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,19 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {useState} from "react";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home/index";
-import TestComponent from "./components/testComponent";
-import {createStore} from "redux";
 import {Provider} from "react-redux";
-
-const initialState = {
-    counter: 0,
-    inputValue: 0
-}
-
-function rootReducer (state = initialState, action) {
-    switch (action.type) {
-        case 'INCREMENT': {
-            return {
-                ...state,
-                counter: state.counter + state.inputValue
-            }
-        }
-        case 'DECREMENT': {
-            return {
-                ...state,
-                counter: state.counter - state.inputValue
-            }
-        }
-        case 'INPUT_CHANGE': {
-            return {
-                ...state,
-                inputValue: Number(action.payload)
-            }
-        }
-
-        default:
-            return {...state};
-
-    }
-}
-
-const store = createStore(rootReducer);
-
+import store from "./store"
 
 function App () {
-    const [user, setUser] = useState(null);
     return (
-        // <BrowserRouter>
-        //     <Routes>
-        //         <Route path={'/'} element={<Home sendUser={setUser}/>}/>
-        //         <Route path={'/messenger'} element={<Dashboard user={user}/>}/>
-        //     </Routes>
-        // </BrowserRouter>
         <Provider store={store}>
-            <TestComponent/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/messenger'} element={<Dashboard/>}/>
+                </Routes>
+            </BrowserRouter>
         </Provider>
-
     );
 }
 
