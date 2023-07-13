3aa983911e629e6194f74a81189047c8e7a75d35
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-07-13 17:27:16 +0300

    Rewrite rootReducer using combineReducers method

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,7 @@
 import ACTION_TYPES from './../actions/types';
+import {combineReducers} from "redux";
+import {chat} from "./chatReducer";
+import {user} from "./userReducer";
 
 const initialState = {
     user: null,
@@ -9,94 +12,9 @@ const initialState = {
     modalWindow: false,
 }
 
-function rootReducer (state = initialState, action) {
-    console.log(action)
-    switch (action.type) {
-        //
-        case ACTION_TYPES.ADD_MESSAGE_REQUEST:
-        case ACTION_TYPES.GET_CURRENT_CHAT_REQUEST:
-        case ACTION_TYPES.GET_ALL_USER_CHATS_REQUEST:
-        case ACTION_TYPES.GET_USER_REQUEST:{
-            return {
-                ...state,
-                isFetching: true
-            }
-        }
-        //
-        case ACTION_TYPES.ADD_MESSAGE_SUCCESS: {
-            return {
-                ...state,
-                currentChat: {
-                    ...state.currentChat,
-                    messages: state.currentChat.messages.concat(action.data)
-                }
-            }
-        }
-        //
-        case ACTION_TYPES.ADD_MESSAGE_ERROR:
-        case ACTION_TYPES.CREATE_NEW_CHAT_ERROR:
-        case ACTION_TYPES.LOGIN_USER_ERROR :
-        case ACTION_TYPES.SIGNUP_USER_ERROR:
-        case ACTION_TYPES.GET_USER_ERROR:
-        case ACTION_TYPES.GET_ALL_USER_CHATS_ERROR:
-        case ACTION_TYPES.UPDATE_USER_ERROR:
-        case ACTION_TYPES.GET_CURRENT_CHAT_ERROR: {
-            return {
-                ...state,
-                errors: action.error
-            }
-        }
-        //
-        case ACTION_TYPES.LOGIN_USER_SUCCESS:
-        case ACTION_TYPES.UPDATE_USER_SUCCESS:
-        case ACTION_TYPES.SIGNUP_USER_SUCCESS: {
-            return {
-                ...state,
-                user: action.data
-            }
-        }
-        //
-        case ACTION_TYPES.GET_ALL_USER_CHATS_SUCCESS: {
-            return {
-                ...state,
-                chatList: action.data
-            }
-        }
-        //
-        case ACTION_TYPES.GET_CURRENT_CHAT_SUCCESS: {
-            return{
-                ...state,
-                currentChat: action.data
-            }
-        }
-        //
-        case ACTION_TYPES.GET_USER_SUCCESS:{
-            return {
-                ...state,
-                user: action.data.user,
-                chatList: action.data.chatList
-            }
-        }
-        //
-        case ACTION_TYPES.LOGOUT:{
-            return {
-                ...initialState
-            }
-        }
-        //
-        case ACTION_TYPES.CREATE_NEW_CHAT_SUCCESS: {
-
-            return{
-                ...state,
-                chatList: action.data
-            }
-        }
-        //
-        default: {
-            return {...state}
-        }
-    }
-}
-
+const rootReducer = combineReducers({
+    user,
+    chat,
+});
 
 export default rootReducer;
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,57 @@
+import ACTION_TYPES from "../actions/types";
+
+const initialState = {
+    user: null,
+    isFetching: false,
+    errors: null,
+}
+
+
+export function user (state = initialState, action){
+    switch (action.type) {
+        //
+        case ACTION_TYPES.GET_USER_REQUEST:{
+            return {
+                ...state,
+                isFetching: true
+            }
+        }
+        //
+        case ACTION_TYPES.LOGIN_USER_SUCCESS:
+        case ACTION_TYPES.UPDATE_USER_SUCCESS:
+        case ACTION_TYPES.SIGNUP_USER_SUCCESS: {
+            return {
+                ...state,
+                user: action.data
+            }
+        }
+        //
+        case ACTION_TYPES.GET_USER_SUCCESS:{
+            return {
+                ...state,
+                user: action.data.user,
+                chatList: action.data.chatList
+            }
+        }
+        //
+        case ACTION_TYPES.LOGOUT:{
+            return {
+                ...initialState
+            }
+        }
+        //
+        case ACTION_TYPES.LOGIN_USER_ERROR :
+        case ACTION_TYPES.SIGNUP_USER_ERROR:
+        case ACTION_TYPES.GET_USER_ERROR:
+        case ACTION_TYPES.UPDATE_USER_ERROR:{
+            return {
+                ...state,
+                errors: action.error
+            }
+        }
+        //
+        default: {
+            return {...state}
+        }
+    }
+}
