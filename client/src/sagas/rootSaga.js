37bf067b00b25206d98a927e9a54592b84c14161
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-07-14 14:57:59 +0300

    Refactor

--- client/src/sagas/rootSaga.js
+++ client/src/sagas/rootSaga.js
@@ -1,29 +1,23 @@
 import {takeLatest} from 'redux-saga/effects';
 import ACTION_TYPES from './../actions/types';
-import {
-    addMessageSaga,
-    createNewChatSaga, deleteChatSaga,
-    deleteMessageSaga,
-    getAllChatsSaga,
-    getCurrentChatWithMessages
-} from './chatSaga';
-import {loginUserSaga, signUpUserSaga, getUserDataSaga, updateUserSaga, logOutSaga} from './userSaga';
+import * as CHAT_SAGA_WORKERS from './chatSaga';
+import * as USER_SAGA_WORKERS from './userSaga';
 
 function* rootSaga () {
     //users sagas
-    yield takeLatest(ACTION_TYPES.LOGIN_USER_REQUEST, loginUserSaga);
-    yield takeLatest(ACTION_TYPES.SIGNUP_USER_REQUEST, signUpUserSaga);
-    yield takeLatest(ACTION_TYPES.GET_USER_REQUEST, getUserDataSaga);
-    yield takeLatest(ACTION_TYPES.UPDATE_USER_REQUEST, updateUserSaga);
-    yield takeLatest(ACTION_TYPES.LOGOUT, logOutSaga);
+    yield takeLatest(ACTION_TYPES.LOGIN_USER_REQUEST, USER_SAGA_WORKERS.loginUserSaga);
+    yield takeLatest(ACTION_TYPES.SIGNUP_USER_REQUEST, USER_SAGA_WORKERS.signUpUserSaga);
+    yield takeLatest(ACTION_TYPES.GET_USER_REQUEST, USER_SAGA_WORKERS.getUserDataSaga);
+    yield takeLatest(ACTION_TYPES.UPDATE_USER_REQUEST, USER_SAGA_WORKERS.updateUserSaga);
+    yield takeLatest(ACTION_TYPES.LOGOUT, USER_SAGA_WORKERS.logOutSaga);
 
     //chats sagas
-    yield takeLatest(ACTION_TYPES.ADD_MESSAGE_REQUEST, addMessageSaga);
-    yield takeLatest(ACTION_TYPES.DELETE_MESSAGE_REQUEST, deleteMessageSaga);
-    yield takeLatest(ACTION_TYPES.DELETE_CHAT_REQUEST, deleteChatSaga);
-    yield takeLatest(ACTION_TYPES.GET_ALL_USER_CHATS_REQUEST, getAllChatsSaga);
-    yield takeLatest(ACTION_TYPES.GET_CURRENT_CHAT_REQUEST, getCurrentChatWithMessages);
-    yield takeLatest(ACTION_TYPES.CREATE_NEW_CHAT_REQUEST, createNewChatSaga);
+    yield takeLatest(ACTION_TYPES.ADD_MESSAGE_REQUEST, CHAT_SAGA_WORKERS.addMessageSaga);
+    yield takeLatest(ACTION_TYPES.DELETE_MESSAGE_REQUEST, CHAT_SAGA_WORKERS.deleteMessageSaga);
+    yield takeLatest(ACTION_TYPES.DELETE_CHAT_REQUEST, CHAT_SAGA_WORKERS.deleteChatSaga);
+    yield takeLatest(ACTION_TYPES.GET_ALL_USER_CHATS_REQUEST, CHAT_SAGA_WORKERS.getAllChatsSaga);
+    yield takeLatest(ACTION_TYPES.GET_CURRENT_CHAT_REQUEST, CHAT_SAGA_WORKERS.getCurrentChatWithMessages);
+    yield takeLatest(ACTION_TYPES.CREATE_NEW_CHAT_REQUEST, CHAT_SAGA_WORKERS.createNewChatSaga);
 }
 
 export default rootSaga;
